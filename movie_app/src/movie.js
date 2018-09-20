889de4b5229590a3f661908f1fb38b55f9841f36
ADMIN-PC\ADMIN <blackdark880523@gmail.com>
2018-09-20 19:54:03 +0900

    functional components

--- movie_app/src/movie.js
+++ movie_app/src/movie.js
@@ -20,18 +20,30 @@ class Movie extends Component {
   }
 }
 
-class MoviePoster extends Component {
+// class MoviePoster extends Component {
 
-    static propTypes = {
-        poster: propTypes.string.isRequired
-    }
+//     static propTypes = {
+//         poster: propTypes.string.isRequired
+//     }
 
-    render() {
-        console.log(this.props.poster);
-        return (
-            <img src={this.props.poster}></img>
-        )
-    }
+//     render() {
+//         //console.log(this.props.poster);
+//         return (
+//             <img src={this.props.poster} />
+//         )
+//     }
+// }
+
+
+//함수형 컴포넌트, state, 라이프 사이클 존재하지 않음, 오로지 return만 존재
+function MoviePoster({poster}){
+    return (
+        <img src={poster} />
+    )
+}
+
+MoviePoster.propTypes = {
+    poster: propTypes.string.isRequired
 }
 
 export default Movie;
